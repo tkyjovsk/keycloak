1ede2cc57b66c6ce246cdf1ecfcafde0c3430e60
Tomas Kyjovsky <tkyjovsk@redhat.com>
2018-12-03 15:15:14 +0100

    KEYCLOAK-9000 Update stress-testing script

--- testsuite/performance/stress-test.sh
+++ testsuite/performance/stress-test.sh
@@ -1,52 +1,65 @@
 #!/bin/bash
 
-BASEDIR=$(cd "$(dirname "$0")"; pwd)
-cd $BASEDIR
+# Execution Parameters
+mvn="${MVN:-mvn}"
+keycloakProjectHome=${KEYCLOAK_PROJECT_HOME:-$(cd "$(dirname "$0")/../.."; pwd)}
+dryRun=${DRY_RUN:-false}
 
-. ./stress-test-config.sh
+# Performance Testsuite Parameters
+dataset=${DATASET:-1r_10c_100u}
+warmUpPeriod=${WARMUP_PERIOD:-120}
+rampUpPeriod=${RAMPUP_PERIOD:-60}
+measurementPeriod=${MEASUREMENT_PERIOD:-120}
+filterResults=${FILTER_RESULTS:-true}
 
-MVN=${MVN:-mvn}
-PROVISIONING_PARAMETERS=${PROVISIONING_PARAMETERS:-}
-PROVISION_COMMAND="$MVN verify -P provision,import-dump $PROVISIONING_PARAMETERS -Ddataset=$dataset"
-TEARDOWN_COMMAND="$MVN verify -P teardown"
+# Stress Test Parameters
+algorithm=${STRESS_TEST_ALGORITHM:-incremental}
+maxIterations=${STRESS_TEST_MAX_ITERATIONS:-10}
+provisioning=${STRESS_TEST_PROVISIONING:-false}
+generateDataset=${STRESS_TEST_PROVISIONING_GENERATE_DATASET:-false}
+provisioningParameters=${STRESS_TEST_PROVISIONING_PARAMETERS:-}
+
+# Stress Test - Incremental Algorithm Parameters
+usersPerSec0=${STRESS_TEST_UPS_FIRST:-1.000}
+incrementFactor=${STRESS_TEST_UPS_INCREMENT:-1.000}
+
+# Stress Test - Bisection Algorithm Parameters
+lowerBound=${STRESS_TEST_UPS_LOWER_BOUND:-0.000}
+upperBound=${STRESS_TEST_UPS_UPPER_BOUND:-10.000}
+resolution=${STRESS_TEST_UPS_RESOLUTION:-1.000}
+
+if $generateDataset; then datasetProfile="generate-data"; else datasetProfile="import-dump"; fi
+provisionCommand="$mvn -f $keycloakProjectHome/testsuite/performance/pom.xml verify -P provision,$datasetProfile $provisioningParameters -Ddataset=$dataset"
+teardownCommand="$mvn -f $keycloakProjectHome/testsuite/performance/pom.xml verify -P teardown"
 
 function runCommand {
     echo "  $1"
     echo 
-    if ! $debug; then eval "$1"; fi
+    if ! $dryRun; then eval "$1"; fi
 }
 
 function runTest {
 
-    # use specified warmUpPeriod only in the first iteration, or if provisioning is enabled
-    if [[ $i == 0 || $provisioning == true ]]; then 
+    if [[ $i == 0 || $provisioning == true ]]; then # use specified warmUpPeriod only in the first iteration, or if provisioning is enabled
         warmUpParameter="-DwarmUpPeriod=$warmUpPeriod ";
     else 
         warmUpParameter="-DwarmUpPeriod=0 ";
     fi
-    if [[ $sequentialUsersFrom == -1 || $provisioning == true ]]; then
-        sequentialUsers=$sequentialUsersFrom
-    else
-        sequentialUsers=`echo "$sequentialUsersFrom * ( $i + 1 )" | bc`
-    fi
 
-    TEST_COMMAND="$MVN verify -Ptest $@ -Ddataset=$dataset $warmUpParameter -DfilterResults=true -DsequentialUsersFrom=$sequentialUsers -DusersPerSec=$usersPerSec"
-
-    echo "ITERATION: $(( i+1 )) / $maxIterations      $ITERATION_INFO"
-    echo 
+    testCommand="$mvn -f $keycloakProjectHome/testsuite/performance/tests/pom.xml verify -Ptest $@ -Ddataset=$dataset $warmUpParameter -DrampUpPeriod=$rampUpPeriod -DmeasurementPeriod=$measurementPeriod -DfilterResults=$filterResults -DusersPerSec=$usersPerSec"
 
     if $provisioning; then 
-        runCommand "$PROVISION_COMMAND"
+        runCommand "$provisionCommand"
         if [[ $? != 0 ]]; then
             echo "Provisioning failed."
-            runCommand "$TEARDOWN_COMMAND" || break
+            runCommand "$teardownCommand" || break
             break
         fi
-        runCommand "$TEST_COMMAND"
+        runCommand "$testCommand"
         export testResult=$?
-        runCommand "$TEARDOWN_COMMAND" || exit 1
+        runCommand "$teardownCommand" || exit 1
     else
-        runCommand "$TEST_COMMAND"
+        runCommand "$testCommand"
         export testResult=$?
     fi
 
@@ -54,25 +67,50 @@ function runTest {
 
 }
 
-
-
-echo "Starting ${algorithm} stress test"
+echo "Stress Test Summary:"
 echo
+echo "Script Execution Parameters:"
+echo "  MVN: $mvn"
+echo "  KEYCLOAK_PROJECT_HOME: $keycloakProjectHome"
+echo "  DRY_RUN: $dryRun"
+echo
+echo "Performance Testsuite Parameters:"
+echo "  DATASET: $dataset"
+echo "  WARMUP_PERIOD: $warmUpPeriod seconds"
+echo "  RAMPUP_PERIOD: $rampUpPeriod seconds"
+echo "  MEASUREMENT_PERIOD: $measurementPeriod seconds"
+echo "  FILTER_RESULTS: $filterResults"
+echo
+echo "Stress Test Parameters:"
+echo "  STRESS_TEST_ALGORITHM: $algorithm"
+echo "  STRESS_TEST_MAX_ITERATIONS: $maxIterations"
+echo "  STRESS_TEST_PROVISIONING: $provisioning"
+if $provisioning; then 
+    echo "  STRESS_TEST_PROVISIONING_GENERATE_DATASET: $generateDataset (mvn -P $datasetProfile)"; 
+    echo "  STRESS_TEST_PROVISIONING_PARAMETERS: $provisioningParameters"; 
+fi
 
-usersPerSecTop=0
+usersPerSecMax=0
 
 case "${algorithm}" in
 
     incremental)
 
+        echo "  STRESS_TEST_UPS_FIRST: $usersPerSec0 users per second"
+        echo "  STRESS_TEST_UPS_INCREMENT: $incrementFactor users per second"
+
         for (( i=0; i < $maxIterations; i++)); do
 
             usersPerSec=`echo "$usersPerSec0 + $i * $incrementFactor" | bc`
 
+            echo
+            echo "ITERATION: $(( i+1 )) / $maxIterations    Load: $usersPerSec users per second"
+            echo 
+
             runTest $@
 
             if [[ $testResult == 0 ]]; then 
-                usersPerSecTop=$usersPerSec
+                usersPerSecMax=$usersPerSec
             else
                 echo "INFO: Last iteration failed. Stopping the loop."
                 break
@@ -84,23 +122,31 @@ case "${algorithm}" in
 
     bisection)
 
+        echo "  STRESS_TEST_UPS_LOWER_BOUND: $lowerBound users per second"
+        echo "  STRESS_TEST_UPS_UPPER_BOUND: $upperBound users per second"
+        echo "  STRESS_TEST_UPS_RESOLUTION: $resolution users per second"
+
         for (( i=0; i < $maxIterations; i++)); do
 
-            intervalSize=`echo "$highPoint - $lowPoint" | bc`
-            usersPerSec=`echo "$lowPoint + $intervalSize * 0.5" | bc`
-            if [[ `echo "$intervalSize < $tolerance" | bc`  == 1 ]]; then echo "INFO: intervalSize < tolerance. Stopping the loop."; break; fi
-            if [[ `echo "$intervalSize < 0" | bc`           == 1 ]]; then echo "ERROR: Invalid state: lowPoint > highPoint. Stopping the loop."; exit 1; fi
-            ITERATION_INFO="L: $lowPoint    H: $highPoint   intervalSize: $intervalSize   tolerance: $tolerance"
+            intervalSize=`echo "$upperBound - $lowerBound" | bc`
+            usersPerSec=`echo "$lowerBound + $intervalSize * 0.5" | bc`
+
+            echo
+            echo "ITERATION: $(( i+1 )) / $maxIterations    Bisection interval: [$lowerBound, $upperBound], intervalSize: $intervalSize, resolution: $resolution,   Load: $usersPerSec users per second"
+            echo 
+
+            if [[ `echo "$intervalSize < $resolution" | bc`  == 1 ]]; then echo "INFO: intervalSize < resolution. Stopping the loop."; break; fi
+            if [[ `echo "$intervalSize < 0" | bc`           == 1 ]]; then echo "ERROR: Invalid state: lowerBound > upperBound. Stopping the loop."; exit 1; fi
 
             runTest $@
 
             if [[ $testResult == 0 ]]; then 
-                usersPerSecTop=$usersPerSec
+                usersPerSecMax=$usersPerSec
                 echo "INFO: Last iteration succeeded. Continuing with the upper half of the interval."
-                lowPoint=$usersPerSec
+                lowerBound=$usersPerSec
             else
                 echo "INFO: Last iteration failed. Continuing with the lower half of the interval."
-                highPoint=$usersPerSec
+                upperBound=$usersPerSec
             fi
 
         done
@@ -114,4 +160,9 @@ case "${algorithm}" in
 
 esac
 
-echo "Highest load with passing test: $usersPerSecTop users per second"
+echo "Maximal achieved load with passing test: $usersPerSecMax users per second"
+
+if ! $dryRun; then # Generate a Jenkins Plot Plugin-compatible data file
+    mkdir -p "$keycloakProjectHome/testsuite/performance/tests/target"
+    echo "YVALUE=$usersPerSecMax" > "$keycloakProjectHome/testsuite/performance/tests/target/stress-test-result.properties"
+fi
